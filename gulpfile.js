4274e5d967e1d015f7e316cc6224aa6079fb93b5
THB-L85 <luis.dr85a@gmail.com>
2025-06-14 18:30:16 -0600

    remove build folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 import {src, dest, watch, series} from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
+import terser from 'gulp-terser'
 
 const sass = gulpSass(dartSass);
 
 export function js( done ){
     src('src/js/app.js')
+        .pipe(terser()) // Minify JavaScript files
         .pipe(dest('build/js'))
 
     done()
@@ -13,7 +15,9 @@ export function js( done ){
 
 export function css( done ){
     src('src/scss/app.scss', {sourcemaps: true}) //! source scss, code here, sourcemaps generate style code map for navigator
-        .pipe(sass().on('error', sass.logError)) // Show errors in console
+        .pipe(sass({
+            style: 'compressed', // compressed style
+        }).on('error', sass.logError)) // Show errors in console
         .pipe(dest('build/css',{sourcemaps: '.'})) // destination css file
 
     done()
